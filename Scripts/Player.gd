166d9b68f96f273c92c207904e95f74226c95476
Murilo Estacio <muriloestacio1@gmail.com>
2024-05-08 22:32:04 -0300

    Criação de um arquivo global

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+var speed = StatusGlobal.player_speed;
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -13,10 +13,10 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
---
+++ Scripts/status_global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var player_life = 20;
+var player_speed = 5;
+var player_xp = 0;
+
+var enemy_speed = 2;
+var enemy_life = 20;
+var enemy_quantity = 1;

5ac9bdb04b512ee39581ce6bbeedf1f9d226b87b
Murilo Estacio <muriloestacio1@gmail.com>
2024-05-08 22:23:57 -0300

    Adição da tela de game over e atualização no inimigo

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,9 +3,9 @@ extends Node3D
 @onready var player = $Player
 
 func _process(delta):
-	if player:
+	if is_instance_valid(player):
 		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 		get_tree().call_group("Inimigos", "update_target_location", player.global_transform.origin)
 	else:
-		get_tree().change_scene_to_file("res://Scenes/pause_menu.tscn")
+		get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
